230cd79b9aae505876ef61faa35930c4ffdd9ef7
famuh <famuh79@gmail.com>
2024-10-09 14:28:23 +0700

    doc : creta watchlist_provider and favorite_provider documentation

--- lib/provider/favorite_provider.dart
+++ lib/provider/favorite_provider.dart
@@ -3,80 +3,92 @@ import 'package:flutter/material.dart';
 import '../common/state_enum.dart';
 import '../data/models/Movie.dart';
 
+/// A provider class that manages the favorite movies state for the application.
+///
+/// This class uses the ChangeNotifier to notify listeners when the favorite
+/// movies list changes, such as when movies are added or removed from the favorites.
 class FavoriteProvider extends ChangeNotifier {
+  // A getter that returns the favorite state
   ResultState? _favoriteState;
-  List<Movie>? _favoriteMovies = [];
-  String _favoriteMessage = '';
-
-  List<Movie>? get favoriteMovies => _favoriteMovies;
-  String get favoriteMessage => _favoriteMessage;
   ResultState? get favoriteState => _favoriteState;
 
+  // A getter that returns [List] of favorite movies
+  List<Movie>? get favoriteMovies => _favoriteMovies;
+  List<Movie>? _favoriteMovies = [];
+
+  // A getter that returns the message
+  String _favoriteMessage = '';
+  String get favoriteMessage => _favoriteMessage;
+
+  /// Adds a movie to the favorites list.
+  /// The [movie] parameter is the movie to be added.
   addMovieToFavorite(Movie movie) async {
     _favoriteState = ResultState.loading;
     notifyListeners();
     try {
-      // Cek apakah movie sudah ada di watchlist
+      // Check if the movie is already in favorites
       bool isMovieInFavorite =
           _favoriteMovies!.any((item) => item.id == movie.id);
       if (isMovieInFavorite) {
-        _favoriteMessage = "Sudah ada didalam favorite";
+        _favoriteMessage = "Already in the favorite";
       } else {
-        // Jika movie belum ada, tambahkan ke watchlist
+        // If the movie is not present, add it to favorites
         _favoriteMovies!.add(movie);
-        _favoriteMessage = "Berhasil menambahkan item ke favorite";
-        print("berhasil tambah favorite");
+        _favoriteMessage = "Successfully added an item to favorites";
       }
     } catch (e) {
-      print(e.toString());
+      // Handle any exceptions that occur
+      debugPrint(e.toString());
     } finally {
       notifyListeners();
     }
   }
 
+  /// A getter that returns whether the movie has been added to favorites.
   bool? _isAddedToFavorite = false;
   bool? get isAddedToFavorite => _isAddedToFavorite;
 
+  /// A getter that returns the current state of checking favorites.
   ResultState? _isFavState;
   ResultState? get isFavState => _isFavState;
 
+  /// Checks if a movie is in the favorites list by its ID.
   checkFavorite(int movieId) async {
     _isFavState = ResultState.loading;
     notifyListeners();
     try {
-      // Cek apakah movie sudah ada di watchlist
+      // Check if the movie is in favorites
       bool isMovieInFavorite =
           _favoriteMovies!.any((item) => item.id == movieId);
 
       if (isMovieInFavorite) {
-        _favoriteMessage = "Sudah ada didalam favorite";
+        _favoriteMessage = "Already in the favorite";
         _isAddedToFavorite = true;
       } else {
-        // Jika movie belum ada, tambahkan ke watchlist
+        // If the movie is not present, indicate it is not added to favorites
         _isAddedToFavorite = false;
-
-        _favoriteMessage = "Berhasil menambahkan item ke favorite";
+        _favoriteMessage = "Not in the favorite";
       }
     } catch (e) {
       _isFavState = ResultState.error;
-      print(e.toString());
+      debugPrint(e.toString());
     } finally {
       notifyListeners();
     }
   }
 
-
-  // Fungsi untuk menghapus movie dari favorite
+  /// Removes a movie from the favorites list.
   removeMovieFromFavorite(Movie movie) async {
     _favoriteState = ResultState.loading;
     notifyListeners();
     try {
+      // Remove the movie from favorites
       _favoriteMovies!.removeWhere((item) => item.id == movie.id);
-      _favoriteMessage = "Berhasil menghapus item dari favorite";
+      _favoriteMessage = "Successfully remove an item from favorites";
       _isAddedToFavorite = false;
-      print("berhasil hapus favorite");
     } catch (e) {
-      print(e.toString());
+      // Handle any exceptions that occur
+      debugPrint(e.toString());
     } finally {
       _favoriteState = ResultState.success;
       notifyListeners();
--- lib/provider/watchlist_provider.dart
+++ lib/provider/watchlist_provider.dart
@@ -1,83 +1,94 @@
 import 'package:flutter/material.dart';
 
 import '../common/state_enum.dart';
-import '../data/api/api_service.dart';
 import '../data/models/Movie.dart';
 
+/// A provider class that manages the watchlist movies state for the application.
+///
+/// This class uses the ChangeNotifier to notify listeners when the watchlist
+/// movies list changes, such as when movies are added or removed from the watchlist.
 class WatchlistProvider extends ChangeNotifier {
+  // A getter that returns the favorite state
   ResultState? _watchlistState;
-  List<Movie>? _watchlistMovies = [];
-  String _watchlistMessage = '';
-
-  List<Movie>? get watchlistMovies => _watchlistMovies;
-  String get watchlistMessage => _watchlistMessage;
   ResultState? get watchlistState => _watchlistState;
 
+  // A getter that returns [List] of favorite movies
+  List<Movie>? _watchlistMovies = [];
+  List<Movie>? get watchlistMovies => _watchlistMovies;
+
+  // A getter that returns the message
+  String _watchlistMessage = '';
+  String get watchlistMessage => _watchlistMessage;
+
+  /// Adds a movie to the watchlist list.
+  /// The [movie] parameter is the movie to be added.
   addMovieToWatchList(Movie movie) async {
     _watchlistState = ResultState.loading;
     notifyListeners();
     try {
-      // Cek apakah movie sudah ada di watchlist
+      // Check if the movie is already in watchlist
       bool isMovieInWatchlist =
           _watchlistMovies!.any((item) => item.id == movie.id);
       if (isMovieInWatchlist) {
-        _watchlistMessage = "Sudah ada didalam watchlist";
+        _watchlistMessage = "Already in the watchlist";
       } else {
-        // Jika movie belum ada, tambahkan ke watchlist
+        // If the movie is not present, add it to watchlist
         _watchlistMovies!.add(movie);
-        _watchlistMessage = "Berhasil menambahkan item ke watchlist";
-        print("berhasil tambah watchlist");
+        _watchlistMessage = "Successfully added an item to watchlist";
       }
     } catch (e) {
-      print(e.toString());
+      // Handle any exceptions that occur
+      debugPrint(e.toString());
     } finally {
       notifyListeners();
     }
   }
 
+  /// A getter that returns whether the movie has been added to watchlist.
   bool? _isAddedToWatchlist = false;
   bool? get isAddedToWatchlist => _isAddedToWatchlist;
 
+  /// A getter that returns the current state of checking watchlist.
   ResultState? _isWLState;
   ResultState? get isWLState => _isWLState;
 
+  /// Checks if a movie is in the watchlist list by its ID.
   checkWatchlist(int movieId) async {
     _isWLState = ResultState.loading;
     notifyListeners();
     try {
-      // Cek apakah movie sudah ada di watchlist
+      // Check if the movie is in watchlist
       bool isMovieInWatchlist =
           _watchlistMovies!.any((item) => item.id == movieId);
 
       if (isMovieInWatchlist) {
-        _watchlistMessage = "Sudah ada didalam watchlist";
+        _watchlistMessage = "Already in the watchlist";
         _isAddedToWatchlist = true;
       } else {
-        // Jika movie belum ada, tambahkan ke watchlist
+        // If the movie is not present, indicate it is not added to watchlist
         _isAddedToWatchlist = false;
-
-        _watchlistMessage = "Berhasil menambahkan item ke watchlist";
+        _watchlistMessage = "Not in the favorite";
       }
     } catch (e) {
       _isWLState = ResultState.error;
-      print(e.toString());
+      debugPrint(e.toString());
     } finally {
       notifyListeners();
     }
   }
 
-  // Fungsi untuk menghapus movie dari favorite
+  /// Removes a movie from the watchlist list.
   removeMovieFromWatchlist(Movie movie) async {
     _watchlistState = ResultState.loading;
     notifyListeners();
     try {
+      // Remove the movie from watchlist
       _watchlistMovies!.removeWhere((item) => item.id == movie.id);
-      _watchlistMessage = "Berhasil menghapus item dari watchlist";
-            _isAddedToWatchlist = false;
-
-      print("berhasil hapus watchlis");
+      _watchlistMessage = "Successfully remove an item from watchlist";
+      _isAddedToWatchlist = false;
     } catch (e) {
-      print(e.toString());
+      // Handle any exceptions that occur
+      debugPrint(e.toString());
     } finally {
       _watchlistState = ResultState.success;
       notifyListeners();
